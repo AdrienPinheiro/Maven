a483303d0d11922152df30b3ffd0cae380df443b
AdrienPinheiro <adrienpnrpro@gmail.com>
2022-06-24 12:45:30 +0200

    Premier commit

---
+++ src/main/java/eu/adrien/maven/SystemInfo.java
@@ -0,0 +1,13 @@
+package eu.adrien.maven;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module eu.adrien.maven {
+    requires javafx.controls;
+    requires javafx.graphics;
+    exports eu.adrien.maven;
+}
